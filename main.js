4b92cbf6da5539ae17333ed51ec6c624d0163391
Mica <micabeacabrera@gmail.com>
2023-02-10 18:37:18 -0300

    odificaciones vista

--- main.js
+++ main.js
@@ -1,75 +1,76 @@
-const $ = (selector)=> document.querySelector(selector);
+const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
-const isHidden = (element)=>element.classList.add('is-hidden');
-const isRemove = (element)=>element.classList.remove('is-hidden');
+const isHidden = (element) => element.classList.add("is-hidden");
+const isRemove = (element) => element.classList.remove("is-hidden");
 
 const BASE_URL = "https://63e0180e59bccf35dabee582.mockapi.io/api";
 let flagEdit = false;
 
-   
 //métodos
+
+//vista index 
+const index = () => {
+  window.location.href = "index.html";
+}
 
 //crear trabajos
 const jobForm = () => {
-    const job = {
-        name: $('#title').value,
-        description:$('#description').value,
-        location:$('#location').value,
-        category:$('#category').value,
-        seniority:$('#seniority').value,
-        };
-    return job
+  const job = {
+    name: $("#title").value,
+    description: $("#description").value,
+    location: $("#location").value,
+    category: $("#category").value,
+    seniority: $("#seniority").value,
+  };
+  return job;
 };
 
-const registerJob = () =>{
-    const job = jobForm();
-    fetch(`${BASE_URL}/jobs`, {
-    method: 'POST',
+const registerJob = () => {
+  const job = jobForm();
+  fetch(`${BASE_URL}/jobs`, {
+    method: "POST",
     headers: {
-    'Content-Type': 'Application/json'
+      "Content-Type": "Application/json",
     },
     body: JSON.stringify(job),
-    })
+  })
     .then((response) => response.json())
-    .then ((data) => {
-        console.log(data)
+    .then((data) => {
+      console.log(data);
     })
-    .catch(() => alert('Error en la base de datos'))
-    .finally(()=> (window.location.href ="index.html"));
-}
+    .catch(() => alert("Error en la base de datos"))
+    .finally(() => (window.location.href = "index.html"));
+};
 
 //obtener todos los trabajos
 const getJobs = () => {
-    fetch(`${BASE_URL}/jobs`)
+  fetch(`${BASE_URL}/jobs`)
     .then((response) => response.json())
-    .then ((jobs) => {
-        nameSelectFilters(jobs);
-        renderJobs(jobs);
-        filterLocation(jobs);  
+    .then((jobs) => {
+      nameSelectFilters(jobs);
+      renderJobs(jobs);
+      filterLocation(jobs);
     })
-    .catch(() => alert('Error en la Api'))
-    
+    .catch(() => alert("Error en la Api"));
 };
 
 getJobs();
 
-//Obtener un trabajo 
+//Obtener un trabajo
 const getJob = (id) => {
-    fetch(`${BASE_URL}/jobs/${id}`)
+  fetch(`${BASE_URL}/jobs/${id}`)
     .then((response) => response.json())
-    .then ((data) => {
-        showSaveDetails(data);
-        infoJobForm(data); 
+    .then((data) => {
+      showSaveDetails(data);
+      infoJobForm(data);
     })
-    .catch(() => alert('Error en la Api'));
+    .catch(() => alert("Error en la Api"));
 };
 
-
-//mostrar cards 
+//mostrar cards
 const renderJobs = (jobs) => {
-
-for(let {id,name,description, location, category,seniority} of jobs) {
-$('.container-cards').innerHTML += `
+  for (let { id, name, description, location, category, seniority } of jobs) {
+    $(".container-cards").innerHTML += `
  <div class="card column is-3 m-2">
  <div class="card-content"> 
  <h1 class="has-text-black">${name}</h1>
@@ -87,15 +88,16 @@ $('.container-cards').innerHTML += `
   </div>
  </div>
  </div>
- `
-}}
+ `;
+  }
+};
 
 //mostrar un trabajo específico
-const showSaveDetails = (data)=> {
-    isHidden($('#view-cards'));
- 
-    $('.details-card').innerHTML = '';
-    $('.details-card').innerHTML += `
+const showSaveDetails = (data) => {
+  isHidden($("#view-cards"));
+
+  $(".details-card").innerHTML = "";
+  $(".details-card").innerHTML += `
     <div class="column is-3 card m-2">
     <div class="card-content"> 
     <h1>${data.name}</h1>
@@ -115,159 +117,156 @@ const showSaveDetails = (data)=> {
     </div>
     </div>
     </div>
-    `
-    //eliminar btn
-    for(const btn of $$('.btn-delete')) {
-    btn.addEventListener('click', ()=> {
-        const idDelete = btn.getAttribute("data-id");
-        deleteJob(idDelete); 
-    })}
-   
-    //editar btn
-    for(const btn of $$('.btn-edit')) {
-    btn.addEventListener('click', ()=> {
-        flagEdit = true;
-
-        const idEdit = btn.getAttribute("data-id"); 
-        isRemove($('#view-form'));
-        $('.btn-create-form').textContent = "Editar";
-        $('.btn-create-form').classList.add('is-primary');
-        $('.btn-create-form').classList.remove('is-link');
-        $('.btn-create-form').setAttribute('data-id',idEdit);
-        getJob(idEdit);
-        //boton cancelar
-        $('.btn-cancel-form').remove();
+    `;
+  //eliminar btn
+  for (const btn of $$(".btn-delete")) {
+    btn.addEventListener("click", () => {
+      const idDelete = btn.getAttribute("data-id");
+      deleteJob(idDelete);
     });
-}
+  }
 
-}
+  //editar btn
+  for (const btn of $$(".btn-edit")) {
+    btn.addEventListener("click", () => {
+      flagEdit = true;
+
+      const idEdit = btn.getAttribute("data-id");
+      isRemove($("#view-form"));
+      $(".btn-create-form").textContent = "Editar";
+      $(".btn-create-form").classList.add("is-primary");
+      $(".btn-create-form").classList.remove("is-link");
+      $(".btn-create-form").setAttribute("data-id", idEdit);
+      getJob(idEdit);
+      //boton cancelar
+      $(".btn-cancel-form").remove();
+    });
+  }
+};
 //eliminar un trabajo
 
 const deleteJob = (id) => {
-    fetch(`${BASE_URL}/jobs/${id}`, {
-        method: 'DELETE',
-    })
-    .finally(()=> (window.location.href ="index.html")); 
+  fetch(`${BASE_URL}/jobs/${id}`, {
+    method: "DELETE",
+  }).finally(() => (window.location.href = "index.html"));
 };
 
 const editJob = (id) => {
-    const job = jobForm();
-    fetch(`${BASE_URL}/jobs/${id}`,{
-    method: 'PUT',
+  const job = jobForm();
+  fetch(`${BASE_URL}/jobs/${id}`, {
+    method: "PUT",
     headers: {
-        "Content-Type": "Application/json",
-     },
+      "Content-Type": "Application/json",
+    },
     body: JSON.stringify(job),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
     })
-    .then((response)=> response.json())
-    .then((data)=>{
-        console.log(data);
-    })
-    .finally(()=>(window.location.href="index.html"));
-}
+    .finally(() => (window.location.href = "index.html"));
+};
 
 // //llenar el formulario de editar con la info del trabajo seleccionado
 const infoJobForm = (data) => {
-    $('#title').value = data.name;
-    $('#description').value = data.description;
-    $('#location').value = data.location;
-    $('#category').value = data.category;
-    $('#seniority').value = data.seniority;      
+  $("#title").value = data.name;
+  $("#description").value = data.description;
+  $("#location").value = data.location;
+  $("#category").value = data.category;
+  $("#seniority").value = data.seniority;
 };
 
 //eventos
-$('#home').addEventListener('click',()=> {
-window.location.href = "index.html";
-//isHidden($('#view-form'));
-})
+$("#home").addEventListener('click',index);
 
-$('#form').addEventListener('submit', (e)=> {
-    e.preventDefault();
+$("#form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-if (flagEdit) {
-    const jobId = $('.btn-create-form').getAttribute('data-id'); 
+  if (flagEdit) {
+    const jobId = $(".btn-create-form").getAttribute("data-id");
     editJob(jobId);
-} else {
+  } else {
     registerJob();
-}
+  }
 
-$('#view-cards').classList.remove('is-hidden');
-$('#view-form').classList.add('is-hidden');
-})
+  $("#view-cards").classList.remove("is-hidden");
+  $("#view-form").classList.add("is-hidden");
+});
 
-//vista Create home, limpiar formulario, cambiar boton
-$('#create-job').addEventListener('click', ()=> {
-    flagEdit = false;
-    isHidden($('#view-cards'));
-    isRemove($('#view-form'));
-    //vaciar formulario
-    $('#title').value = " ";
-    $('#description').value = " ";
-    $('#location').value = " ";
-    $('#category').value = " ";
-    $('#seniority').value = " ";
-    $('.details-card').innerHTML = '';
+//vista Create Job
+$("#create-job").addEventListener("click", () => {
+  flagEdit = false;
+  isHidden($("#view-cards"));
+  isRemove($("#view-form"));
 
-    $('.btn-create-form').textContent = "Create";
-    $('.btn-create-form').classList.remove('is-primary');
-    $('.btn-create-form').classList.add('is-link');
-    $('.btn-create-form').removeAttribute('data-id',idEdit);
-  })
-  
+  $(".btn-create-form").textContent = "Create";
+  $(".btn-create-form").classList.remove("is-primary");
+  $(".btn-create-form").classList.add("is-link");
+  $(".btn-create-form").removeAttribute("data-id", idEdit);
+});
 
 //filtros
-//arreglar q no se vea el Seniority...
-
 const nameSelectFilters = (jobs) => {
-const filterLocation = jobs.map((location)=> location.location);
-const filterLocationSet = new Set(filterLocation);
+  const filterLocation = jobs.map((location) => location.location);
+  const filterLocationSet = new Set(filterLocation);
 
-const filterSeniority = jobs.map((seniority)=> seniority.seniority);
-const filterSenioritySet = new Set(filterSeniority);
+  const filterSeniority = jobs.map((seniority) => seniority.seniority);
+  const filterSenioritySet = new Set(filterSeniority);
 
-const filterCategory = jobs.map((category)=> category.category);
-const filterCategorySet = new Set(filterCategory);
+  const filterCategory = jobs.map((category) => category.category);
+  const filterCategorySet = new Set(filterCategory);
 
-for (let location of filterLocationSet) {
- $('#select-location').innerHTML += `<option value="${location}"> ${location} </option>`
-}
+  for (let location of filterLocationSet) {
+    $(
+      "#select-location"
+    ).innerHTML += `<option value="${location}"> ${location} </option>`;
+  }
 
-for(let seniority of filterSenioritySet) {
-$('#select-seniority').innerHTML += `<option value="${seniority}"> ${seniority} </option>`
-}
+  for (let seniority of filterSenioritySet) {
+    $(
+      "#select-seniority"
+    ).innerHTML += `<option value="${seniority}"> ${seniority} </option>`;
+  }
 
-for(let category of filterCategorySet) {
-    $('#select-category').innerHTML += `<option value="${category}"> ${category} </option>`
-    }
-}
+  for (let category of filterCategorySet) {
+    $(
+      "#select-category"
+    ).innerHTML += `<option value="${category}"> ${category} </option>`;
+  }
+};
 
-
-//busca x filtro 
+//busca x filtro
 const filterLocation = (jobs) => {
-    $('#btn-search').addEventListener('click', ()=> {
-    const valueLocation = $('#select-location').value;
-    const valueSeniority =$('#select-seniority').value;
-    const valueCategory = $('#select-category').value;
-     
-    let filterJobs =  [...jobs];
-    filterJobs = filterJobs.filter((job)=> job.location === valueLocation);
-    filterJobs = filterJobs.filter((job)=>job.seniority === valueSeniority);
-    filterJobs = filterJobs.filter((job)=> job.category === valueCategory);
-    console.log(filterJobs);
+  $("#btn-search").addEventListener("click", () => {
+    const valueLocation = $("#select-location").value;
+    const valueSeniority = $("#select-seniority").value;
+    const valueCategory = $("#select-category").value;
+
+    let filterJobs = [...jobs];
+
     
-    if(filterJobs.length > 0) {
-        filterJobs = filterJobs.filter((job)=> job.location === valueLocation);
-        filterJobs = filterJobs.filter((job)=>job.seniority === valueSeniority);
-        filterJobs = filterJobs.filter((job)=> job.category === valueCategory);
-        renderFilters(filterJobs);
+
+    if (valueLocation !== "Location") {
+      filterJobs = filterJobs.filter((job) => job.location === valueLocation);
     }
-   else if (filterJobs.length === 0) {
-    $('.container-cards').innerHTML ='No se encontraron resultados' };
-    })
-}
+    if (valueSeniority !== "Seniority") {
+      filterJobs = filterJobs.filter((job) => job.seniority === valueSeniority);
+    }
+    if (valueCategory !== "Category") {
+      filterJobs = filterJobs.filter((job) => job.category === valueCategory);
+    }
+
+    console.log(filterJobs);
+    renderFilters(filterJobs);
+  });
+};
 
 //muestra los trabajos filtrados
 const renderFilters = (filterJobs) => {
-$('.container-cards').innerHTML ='';
-renderJobs(filterJobs);
-}
+  $(".container-cards").innerHTML = "";
+  renderJobs(filterJobs);
+};
+
+// limpia los filtros
+$('#btn-clear').addEventListener('click',index);
+
